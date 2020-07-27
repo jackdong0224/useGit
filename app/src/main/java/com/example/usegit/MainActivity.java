a0babef2b1caedb856541a4e88deb2a8b1929bdb
jackdong0224 <dongchengcheng0224@163.com>
2020-07-27 16:24:46 +0800

    修改了打印的值

--- app/src/main/java/com/example/usegit/MainActivity.java
+++ app/src/main/java/com/example/usegit/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         //在git修改了,在本地测试一下吧 我又在remote修改了
         //在git修改了,在本地测试一下吧 我是在local修改的123
         if (false){
-            System.out.println("打印true111");
+            System.out.println("打印true222");
         }
     }
 }
