7ef5c02ee013d8de3c880d7c52d7e18f8344cf8a
jackdong0224 <dongchengcheng0224@163.com>
2020-07-28 09:02:24 +0800

    开始使用kotlin

---
+++ app/src/main/java/com/example/usegit/MyKotlin.kt
@@ -0,0 +1,13 @@
+package com.example.usegit
+
+fun main(args: ArrayList<String>) {
+    System.out.println("helloworld kotlin")
+    //var 代表变量 kotlin可以自己推断数据类型
+    //val代表常量 不可修改
+    var a: Byte = Byte.MAX_VALUE
+    var b: Byte = Byte.MIN_VALUE
+    var c: Long = Long.MAX_VALUE
+    var d: Long = Long.MIN_VALUE
+
+
+}
